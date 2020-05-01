7fdde4ddb57e2a9277c051a3cf74311f1a01dfd1
Karmello <nogakamil@vp.pl>
2020-05-02 00:52:56 +0200

    sdfsdf

--- src/mocks/questions.ts
+++ src/mocks/questions.ts
@@ -1,5 +1,6 @@
 import faker from 'faker'
 import times from 'lodash/times'
+
 import { MIN_NUM_OF_ANSWERS, MAX_NUM_OF_ANSWERS } from 'shared/utils/index'
 import { getRandNum } from 'shared/helpers/index'
 
--- src/mocks/seed.ts
+++ src/mocks/seed.ts
@@ -1,7 +1,9 @@
 import { MongoClient } from 'mongodb'
 import questionMocks from './questions'
 
-const uri = 'mongodb://localhost:27017/ask-the-world-local'
+// const uri = 'mongodb://localhost:27017/ask-the-world-local'
+const uri =
+  'mongodb://heroku_kfmm9zz8:iv9vvsm77jpl9ha1u30e7ambta@ds259361.mlab.com:59361/heroku_kfmm9zz8'
 
 const resetDB = async (client: MongoClient) => {
   const questionsCollection = await client.db().collection('questions')
