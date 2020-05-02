3aae9de0fd0c84ce3f51129b07fef798ee9e2a1f
Karmello <nogakamil@vp.pl>
2020-05-02 15:36:01 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from 0e29da2..5b28cd3
    5b28cd3 added ReadTopQuestions
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 5b28cd3a0ae08bcabb0759be1bcc9f6bcba87fa6

--- utils/enums/ApiUrlPath.ts
+++ utils/enums/ApiUrlPath.ts
@@ -1,6 +1,7 @@
 enum ApiUrlPath {
   CreateQuestion = "/create-question",
   ReadQuestions = "/read-questions",
+  ReadTopQuestions = "/read-top-questions",
   ReadQuestion = "/read-question",
   UpdateQuestion = "/update-question",
 }
