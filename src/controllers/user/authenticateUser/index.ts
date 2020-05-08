07a863011b53c66f33e9816875d5b69e7fe0666b
Karmello <nogakamil@vp.pl>
2020-05-09 01:32:19 +0200

    auth user controller

--- src/controllers/user/authenticateUser/index.ts
+++ src/controllers/user/authenticateUser/index.ts
@@ -1,9 +1,14 @@
 import { Application, Request, Response } from 'express'
 
+import { UserModel } from 'models/index'
 import { ApiUrlPath } from 'shared/utils/index'
 
 export default (app: Application) =>
   app.post(ApiUrlPath.AuthenticateUser, async (req: Request, res: Response) => {
     //
-    res.status(200).send()
+    UserModel.findOne({
+      username: req.body.username,
+    })
+      .then(doc => res.status(200).send(doc))
+      .catch(err => res.status(400).send(err))
   })
