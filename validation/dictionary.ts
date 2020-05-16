b9923a4f10c1bfb578b6a3f11ad1a806278f1e60
Karmello <nogakamil@vp.pl>
2020-05-16 21:26:54 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from c4e2adb..08fba26
    08fba26 dictionary
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 08fba2672c6d7d46f43d63c92a1a70ba5ca298f1

---
+++ validation/dictionary.ts
@@ -0,0 +1,8 @@
+export default {
+  requiredMsg: 'is required',
+  invalidCharMsg: 'contains invalid character(s)',
+  invalidMsg: 'is invalid',
+  incorrectMsg: 'is incorrect',
+  getMinLengthMsg: (min: number) => `must be ${min} character${min !== 1 ? 's' : ''} at min`,
+  getMaxLengthMsg: (max: number) => `must be ${max} character${max !== 1 ? 's' : ''} at max`,
+}
