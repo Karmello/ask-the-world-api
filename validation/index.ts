90b4fd8d9db3f7fa70ca12bdbd9efdad7b91643b
Karmello <nogakamil@vp.pl>
2020-06-06 03:17:59 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from 2fc5701..14833be
    14833be validation
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 14833be7aaafe23b8405821b21edb377991c6aaf

--- validation/index.ts
+++ validation/index.ts
@@ -1,15 +1,20 @@
 export const isDefined = (value?: string): boolean => Boolean(value)
 
-export const isNotTooShort = (value: string, min: number): boolean => value && value.length >= min
+export const isStringNotTooShort = (value: string, min: number): boolean =>
+  value && value.length >= min
 
-export const isNotTooLong = (value: string, max: number): boolean => value && value.length <= max
+export const isStringNotTooLong = (value: string, max: number): boolean =>
+  value && value.length <= max
 
-export const doesNotContainAlphaChars = (value: string, isPassword?: boolean): boolean =>
+export const doesStringNotContainAlphaChars = (value: string, isPassword?: boolean): boolean =>
   (isPassword && new RegExp(/^([a-zA-Z0-9_!@#$%-+=?]+)$/).test(value)) ||
   new RegExp(/^([a-zA-Z0-9_]+)$/).test(value)
 
 export const isValidEmail = (value: string): boolean =>
   new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/).test(value)
 
-export const isSameOrBefore = (value: string, maxDate: string): boolean =>
+export const isDateSameOrBefore = (value: string, maxDate: string): boolean =>
   new Date(value) <= new Date(maxDate)
+
+export const isNumberWithinBoundaries = (value: number, min: number, max: number): boolean =>
+  value >= min && value <= max
