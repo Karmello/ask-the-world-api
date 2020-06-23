fb4a299f1319e44fdbe747da6b197ede42628815
Karmello <nogakamil@vp.pl>
2020-06-23 21:23:28 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from 7b62203..0651a4e
    0651a4e update user api url path
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 0651a4e24f6be89de7883e7fd25925e3a8e900fe

--- utils/enums/ApiUrlPath.ts
+++ utils/enums/ApiUrlPath.ts
@@ -2,6 +2,7 @@ enum ApiUrlPath {
   AuthenticateUser = '/authentication',
   RegisterUser = '/registration',
   ReadUser = '/read-user',
+  UpdateUser = '/update-user',
   CreateQuestion = '/create-question',
   ReadQuestions = '/read-questions',
   ReadTopQuestions = '/read-top-questions',
