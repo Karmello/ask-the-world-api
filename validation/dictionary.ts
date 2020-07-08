06df2211f5e35bda2b6100ee77c2acb196db3e28
Karmello <nogakamil@vp.pl>
2020-07-08 19:25:20 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from 9212d04..a54de0a
    a54de0a dictionary
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: a54de0a119c00e9c083226e6383c47328347f9a4

--- validation/dictionary.ts
+++ validation/dictionary.ts
@@ -4,6 +4,7 @@ export default {
   invalidMsg: 'is invalid',
   incorrectMsg: 'is incorrect',
   incorrectCredentialsMsg: 'Incorrect credentials',
+  incorrectPassword: 'Incorrect password',
   alreadyTakenMsg: 'is already taken',
   getMinLengthMsg: (min: number) => `must be ${min} character${min !== 1 ? 's' : ''} at min`,
   getMaxLengthMsg: (max: number) => `must be ${max} character${max !== 1 ? 's' : ''} at max`,
