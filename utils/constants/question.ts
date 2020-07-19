29bd8f17a0d2886cbd7e89c14a2e9024e38800aa
Karmello <nogakamil@vp.pl>
2020-07-19 14:01:47 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from 5a07372..f18cd3a
    f18cd3a SEARCH_QUESTIONS_MAX
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: f18cd3a0543bb0744a0ec066ae7c2fbc1cd2422e

--- utils/constants/question.ts
+++ utils/constants/question.ts
@@ -1,4 +1,5 @@
 export const READ_QUESTIONS_MAX = 25
+export const SEARCH_QUESTIONS_MAX = 25
 export const NUM_OF_TOP_QUESTIONS = 25
 export const MIN_NUM_OF_ANSWERS = 2
 export const MAX_NUM_OF_ANSWERS = 10
