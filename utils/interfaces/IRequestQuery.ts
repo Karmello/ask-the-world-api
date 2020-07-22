b64987d8bfee1d657fee1efd3346879d2590ffe8
Karmello <nogakamil@vp.pl>
2020-07-23 01:20:04 +0200

    Squashed 'src/lib/ask-the-world-shared/' changes from 9a8cb7d..1a7b0b9
    1a7b0b9 sdf
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 1a7b0b91dcd4087c6bf29c9099023cc049427cc4

--- utils/interfaces/IRequestQuery.ts
+++ utils/interfaces/IRequestQuery.ts
@@ -1,13 +1,9 @@
 interface IRequestQuery {
-  query?: {
-    userId?: string
-  }
+  userId?: string
   skip?: number
   limit?: number
-  sort?: {
-    timestamp?: number
-    answeredTimes?: number
-  }
+  timestamp?: number
+  answeredTimes?: number
 }
 
 export default IRequestQuery
