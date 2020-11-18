298b5db0d1dadf2c64d7b5c30fb93cdae52e28e7
Karmello <nogakamil@vp.pl>
2020-11-18 04:45:17 +0100

    mongo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
         NODE_ENV: 'production',
         REACT_APP_ENV: 'remote-dev',
         MONGO_URI:
-          'mongodb://admin:Jordan#23@cluster0-shard-00-00.wrpq7.mongodb.net:27017,cluster0-shard-00-01.wrpq7.mongodb.net:27017,cluster0-shard-00-02.wrpq7.mongodb.net:27017/ask-the-world-dev?ssl=true&replicaSet=atlas-1o0xqx-shard-0&authSource=admin&retryWrites=true&w=majority',
+          'mongodb+srv://admin:Jordan#23@cluster0.wrpq7.mongodb.net/ask-the-world-dev?retryWrites=true&w=majority',
       },
     },
   ],
