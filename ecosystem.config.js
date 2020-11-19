53be392719f999106702478dcaf8f30220ef81a8
Karmello <nogakamil@vp.pl>
2020-11-19 04:15:07 +0100

    added AUTH_SECRET

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,7 @@ module.exports = {
         REACT_APP_ENV: 'remote-dev',
         MONGO_URI:
           'mongodb+srv://admin:25Xe5fYhARFobS1d@cluster0.wrpq7.mongodb.net/ask-the-world-dev?retryWrites=true&w=majority',
+        AUTH_SECRET: 'lifeislikeaslideshow',
       },
     },
   ],
