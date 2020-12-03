728e4d9eb98a228939d817409f02af06d332d0ff
Karmello <nogakamil@vp.pl>
2020-12-03 14:14:29 +0100

    info

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -45,10 +45,8 @@ module.exports = {
       ],
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        CI_COMMIT_REF_NAME,
-        CI_COMMIT_SHA,
-      },
+      'process.env.CI_COMMIT_REF_NAME': CI_COMMIT_REF_NAME,
+      'process.env.CI_COMMIT_SHA': CI_COMMIT_SHA,
     }),
   ],
 }
