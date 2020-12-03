3b135a431132d5a3451d807c2867b98681ae7ea1
Karmello <nogakamil@vp.pl>
2020-12-03 12:51:18 +0100

    ci env vars

--- src/controllers/other/ReadInfo/index.ts
+++ src/controllers/other/ReadInfo/index.ts
@@ -6,7 +6,7 @@ export default (app: Application) =>
   app.get(ApiUrlPath.ReadInfo, (req: Request, res: Response) =>
     res.status(200).send({
       status: 'OK',
-      branchName: process.env.BRANCH_NAME,
+      branchName: process.env.CI_COMMIT_REF_NAME,
       revision: process.env.COMMIT_HASH,
     })
   )
