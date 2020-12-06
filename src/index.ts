24c409027ebb026b776fc4e330de8579889667d7
Karmello <nogakamil@vp.pl>
2020-12-06 16:29:24 +0100

    changing to just 2 ec2 instances

--- src/index.ts
+++ src/index.ts
@@ -59,9 +59,9 @@ mongoose
       if (![Env.Local, Env.Test].includes(APP_ENV as Env)) {
         createServer(
           {
-            key: readFileSync(path.resolve('./../../ssl/key.pem'), { encoding: 'utf-8' }),
-            cert: readFileSync(path.resolve('./../../ssl/cert.pem'), { encoding: 'utf-8' }),
-            ca: readFileSync(path.resolve('./../../ssl/ca.pem'), { encoding: 'utf-8' }),
+            key: readFileSync(path.resolve('./../ssl/key.pem'), { encoding: 'utf-8' }),
+            cert: readFileSync(path.resolve('./../ssl/cert.pem'), { encoding: 'utf-8' }),
+            ca: readFileSync(path.resolve('./../ssl/ca.pem'), { encoding: 'utf-8' }),
             passphrase: 'zH3N3K4DKY',
           },
           app
