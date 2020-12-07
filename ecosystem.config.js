eb8fec1a1d8361be0bcacf91f60a1ecf57edad50
Karmello <nogakamil@vp.pl>
2020-12-07 14:09:40 +0100

    cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: 'ask-the-world-api',
       script: 'index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
       env: {
         PORT: 9000,
         NODE_ENV: 'production',
