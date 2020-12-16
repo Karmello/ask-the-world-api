ae8f2b827f7128e022bc90277ceec5c858f0c164
Karmello <nogakamil@vp.pl>
2020-12-16 21:14:46 +0100

    Squashed 'src/lib/ask-the-world-shared/' changes from f8654c1..32f4f14
    32f4f14 filter
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 32f4f14005564b0f94148b5af29958a269d4bb32

--- utils/enums/Filter.ts
+++ utils/enums/Filter.ts
@@ -1,8 +1,8 @@
 enum Filter {
   All = 'ALL',
   Any = 'ANY',
-  CreatedByYou = 'CREATED_BY_YOU',
-  EditedByYou = 'EDITED_BY_YOU',
+  Created = 'Created',
+  Answered = 'Answered',
 }
 
 export default Filter
