f632ddc2473680fdfb4d7cfba19f643ffa811883
Karmello <nogakamil@vp.pl>
2020-12-19 22:27:39 +0100

    GetQuestions

--- src/controllers/question/GetQuestions/index.ts
+++ src/controllers/question/GetQuestions/index.ts
@@ -1,38 +1,60 @@
 import { Application, Request, Response } from 'express'
 
+import { ApiUrlPath, IRequestQuery, Filter, READ_QUESTIONS_MAX } from 'shared/utils/index'
 import { userAuthMiddleware } from 'middleware/index'
-import { ApiUrlPath, IRequestQuery, Filter } from 'shared/utils/index'
 import { QuestionModel } from 'models/index'
+import SortBy from 'shared/utils/enums/SortBy'
 
 export default (app: Application) =>
   app.get(ApiUrlPath.GetQuestions, userAuthMiddleware, (req: Request, res: Response) => {
     //
     const {
       userId,
-      skip = 0,
-      limit = 0,
-      timestamp,
-      answeredTimes,
+      filter,
+      sortBy,
+      pageNo,
       keywords,
       keywordsMode,
     } = (req.query as unknown) as IRequestQuery
 
-    const selfAnswered = Number(req.query.selfAnswered)
-    const query = {} as any
+    if (
+      // not authorized, filter other than ALL
+      (!req.decoded?._id && filter !== Filter.All) ||
+      // authorized, requesting someone else's questions, filter other than CREATED
+      (req.decoded?._id && req.decoded?._id !== userId && filter !== Filter.Created)
+    ) {
+      return res.status(400).send()
+    }
+
+    const skip = (Number(pageNo) - 1) * READ_QUESTIONS_MAX
+    const limit = READ_QUESTIONS_MAX
     const sort = {} as any
+    const query = {} as any
 
-    if (userId) query.userId = userId
-    if (timestamp) sort.timestamp = Number(timestamp)
-    if (answeredTimes) sort.answeredTimes = Number(answeredTimes)
+    switch (sortBy) {
+      case SortBy.DateCreated:
+        sort.timestamp = -1
+        break
+      case SortBy.MostPopular:
+        sort.answeredTimes = -1
+        break
+    }
 
-    if (selfAnswered) {
-      delete query.userId
-      const $elemMatch = {
-        votes: {
-          $in: req.decoded?._id,
-        },
-      }
-      query.answers = selfAnswered === -1 ? { $not: { $elemMatch } } : { $elemMatch }
+    switch (filter) {
+      case Filter.Created:
+        if (userId) query.userId = userId
+        break
+      case Filter.Answered:
+        query.answers = {
+          $elemMatch: { votes: { $in: req.decoded?._id } },
+        }
+        break
+      case Filter.NotAnswered:
+        query.answers = { $not: { $elemMatch: { votes: { $in: req.decoded?._id } } } }
+        break
+      case Filter.Watched:
+        query.watchers = { $elemMatch: { $in: req.decoded?._id } }
+        break
     }
 
     if (keywords) {
@@ -57,6 +79,9 @@ export default (app: Application) =>
           count: results[0],
           data: QuestionModel.transformBeforeSend(results[1], req.decoded?._id),
         }),
-      err => res.status(400).send(err)
+      err => {
+        console.log(err)
+        res.status(400).send(err)
+      }
     )
   })
