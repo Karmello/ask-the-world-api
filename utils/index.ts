e4acf16284a422199f1b548f43f284a39d814e25
Karmello <nogakamil@vp.pl>
2021-01-23 17:16:20 +0100

    Squashed 'src/lib/ask-the-world-shared/' changes from 4cc47753..5e0aea48
    5e0aea48 Error anum
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 5e0aea4860b7b0d94395b38f2c8fa57017c1a721

--- utils/index.ts
+++ utils/index.ts
@@ -2,6 +2,7 @@ export * from './constants/index'
 
 export { default as ApiUrlPath } from './enums/ApiUrlPath'
 export { default as Env } from './enums/Env'
+export { default as Error } from './enums/Error'
 export { default as Filter } from './enums/Filter'
 export { default as HttpMethod } from './enums/HttpMethod'
 export { default as ReportReason } from './enums/ReportReason'
