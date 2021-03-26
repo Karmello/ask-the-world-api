6dd6e544e38955d79b1ef9c0c683671fdc45a3bd
Karmello <nogakamil@vp.pl>
2021-03-26 12:01:22 +0100

    Squashed 'src/lib/ask-the-world-shared/' changes from bf6dd813..48ee4aa9
    48ee4aa9 updated IUser
    
    git-subtree-dir: src/lib/ask-the-world-shared
    git-subtree-split: 48ee4aa94f9c073dfcc194a8b6687c2f0698ee22

--- utils/interfaces/IUser.ts
+++ utils/interfaces/IUser.ts
@@ -1,14 +1,14 @@
 import { Sex } from './../index'
 interface IUser {
   _id: any
-  active: boolean
   email: string
   username: string
   password: string
   dateOfBirth: string
   country: string
   sex: Sex
-  timestamp: number
+  registeredAt: number
+  active: boolean
 }
 
 export default IUser
