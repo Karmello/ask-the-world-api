2792a55c77bef90b6e0558ffad73a357a28d4972
Karmello <nogakamil@vp.pl>
2021-03-31 18:58:01 +0200

    User

--- src/models/User/User.spec.ts
+++ src/models/User/User.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import { UserModel } from 'models/index'
 
 describe('UserModel', () => {
+  //
   describe('email', () => {
     //
     it('required', () => {
@@ -36,10 +37,45 @@ describe('UserModel', () => {
       expect(err.errors.email.kind).to.eql('checkEmail')
     })
 
+    it('checkEmail', () => {
+      const user = new UserModel({
+        email: 'user_user_user_user_user_user_user_user_user_user@gmail.com',
+      })
+      const err = user.validateSync()
+      expect(err.errors.email.kind).to.eql('checkMaxLength')
+    })
+
     it('OK', () => {
       const user = new UserModel({ email: 'user@gmail.com' })
       const err = user.validateSync()
       expect(err.errors.email).to.be.undefined
+    })
+
+    it('OK', () => {
+      const user = new UserModel({ email: 'user_user_user_user@gmail.com' })
+      const err = user.validateSync()
+      expect(err.errors.email).to.be.undefined
+    })
+  })
+
+  describe('username', () => {
+    //
+    it('required', () => {
+      const user = new UserModel()
+      const err = user.validateSync()
+      expect(err.errors.username.kind).to.eql('required')
+    })
+
+    it('required', () => {
+      const user = new UserModel({ username: null })
+      const err = user.validateSync()
+      expect(err.errors.username.kind).to.eql('required')
+    })
+
+    it('required', () => {
+      const user = new UserModel({ username: '' })
+      const err = user.validateSync()
+      expect(err.errors.username.kind).to.eql('required')
     })
   })
 })
