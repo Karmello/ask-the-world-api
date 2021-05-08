2c5e83eefbc306b1c7c34f9bf3b7cb0fb5a4a057
Karmello <nogakamil@vp.pl>
2021-05-08 16:13:19 +0200

    improving GetQuestions

--- src/controllers/question/GetQuestions/index.ts
+++ src/controllers/question/GetQuestions/index.ts
@@ -1,8 +1,11 @@
 import { Application, Request, Response } from 'express'
+import mongoose from 'mongoose'
 
 import { ApiUrlPath, IRequestQuery, Filter, SortBy, READ_QUESTIONS_MAX } from 'shared/utils/index'
 import { userAuthMiddleware } from 'middleware/index'
 import { QuestionModel } from 'models/index'
+
+const ObjectId = mongoose.Types.ObjectId
 
 export default (app: Application) =>
   app.get(ApiUrlPath.GetQuestions, userAuthMiddleware, (req: Request, res: Response) => {
@@ -28,11 +31,11 @@ export default (app: Application) =>
     const skip = (Number(pageNo) - 1) * READ_QUESTIONS_MAX
     const limit = READ_QUESTIONS_MAX
     const sort = {} as any
-    const query = {} as any
+    const match = {} as any
 
     switch (sortBy) {
       case SortBy.DateCreated:
-        sort.timestamp = -1
+        sort.createdAt = -1
         break
       case SortBy.MostPopular:
         sort.answeredTimes = -1
@@ -41,46 +44,68 @@ export default (app: Application) =>
 
     switch (filter) {
       case Filter.Created:
-        if (userId) query.creatorId = userId
+        if (userId) match.creatorId = ObjectId(userId)
         break
       case Filter.Answered:
-        query.answers = {
-          $elemMatch: { votes: { $in: req.decoded?._id } },
+        match.answers = {
+          $elemMatch: {
+            votes: {
+              $in: ObjectId(req.decoded?._id),
+            },
+          },
         }
         break
       case Filter.NotAnswered:
-        query.answers = { $not: { $elemMatch: { votes: { $in: req.decoded?._id } } } }
+        match.answers = {
+          $not: {
+            $elemMatch: {
+              votes: {
+                $in: ObjectId(req.decoded?._id),
+              },
+            },
+          },
+        }
         break
       case Filter.Watched:
-        query.watchers = { $elemMatch: { $in: req.decoded?._id } }
+        match.watchers = {
+          $elemMatch: {
+            $in: ObjectId(req.decoded?._id),
+          },
+        }
         break
     }
 
     if (keywords) {
       if (keywordsMode === Filter.All) {
-        query.text = {
+        match.text = {
           $all: keywords.split(' ').map(word => new RegExp(word, 'i')),
         }
       } else if (keywordsMode === Filter.Any) {
-        query.text = {
+        match.text = {
           $regex: keywords.split(' ').join('|'),
           $options: 'i',
         }
       }
     }
 
-    Promise.all([
-      QuestionModel.countDocuments(query),
-      QuestionModel.find(query).sort(sort).skip(Number(skip)).limit(Number(limit)).lean(true),
+    QuestionModel.aggregate([
+      {
+        $facet: {
+          allDocs: [{ $count: 'count' }],
+          matchedDocs: [
+            { $match: match },
+            { $sort: sort },
+            { $skip: Number(skip) },
+            { $limit: Number(limit) },
+          ],
+        },
+      },
     ]).then(
       results =>
         res.status(200).send({
-          count: results[0],
-          data: results[1],
+          count: results[0].allDocs[0].count,
+          data: results[0].matchedDocs,
         }),
-      err => {
-        console.log(err)
-        res.status(400).send(err)
-      }
+      err => res.status(400).send(err)
     )
   })
