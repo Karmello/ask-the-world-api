dffb396c600265cedcde620f92fc39e7a948e331
Karmello <nogakamil@vp.pl>
2021-05-10 22:38:11 +0200

    pull-shared

--- src/controllers/other/ReadStats/index.ts
+++ src/controllers/other/ReadStats/index.ts
@@ -1,7 +1,7 @@
 import { Application, Request, Response } from 'express'
 
 import { userAuthMiddleware } from 'middleware/index'
-import { ApiUrlPath, Sex } from 'shared/utils/index'
+import { ApiUrlPath } from 'shared/utils/index'
 import { UserModel, QuestionModel, AnswerModel } from 'models/index'
 
 export default (app: Application) =>
@@ -11,8 +11,6 @@ export default (app: Application) =>
       UserModel.countDocuments(),
       QuestionModel.countDocuments(),
       AnswerModel.countDocuments(),
-      UserModel.countDocuments({ sex: Sex.Female }),
-      UserModel.countDocuments({ sex: Sex.Male }),
     ]).then(
       results =>
         res.status(200).send({
@@ -20,8 +18,6 @@ export default (app: Application) =>
             users: results[0],
             questions: results[1],
             answers: results[2],
-            females: results[3],
-            males: results[4],
           },
         }),
       err => res.status(400).send(err)
