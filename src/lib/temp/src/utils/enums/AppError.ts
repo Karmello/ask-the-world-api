64cf415648380f45ebe22cdd091a365e660b09de
Karmello <nogakamil@vp.pl>
2021-05-11 18:14:39 +0200

    pull shared

--- src/lib/temp/src/utils/enums/AppError.ts
+++
@@ -1,8 +0,0 @@
-enum AppError {
-  SessionExpired = 'SESSION_EXPIRED_ERROR',
-  NoSuchUserError = 'NO_SUCH_USER_ERROR',
-  NoSuchQuestionError = 'NO_SUCH_QUESTION_ERROR',
-  SomethingWentWrong = 'SOMETHING_WENT_WRONG',
-}
-
-export default AppError
