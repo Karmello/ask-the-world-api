e8e1f0116ce81f04fead66ebbd6724fdc12c5505
Karmello <nogakamil@vp.pl>
2021-05-12 01:01:35 +0200

    aws update

--- aws/scripts/start.sh
+++ aws/scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/ask-the-world-api
 
+pm2 delete ask-the-world-api
+
 if [ "$APPLICATION_NAME" == "ask-the-world-api-feature" ]
 then
   pm2 start ecosystem.config.js --env feature
