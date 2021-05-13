280ed29868b97cf5c095908bb6f04f8918a65955
Karmello <nogakamil@vp.pl>
2021-05-13 23:07:07 +0200

    Squashed 'src/lib/temp/' content from commit 6f4929c
    git-subtree-dir: src/lib/temp
    git-subtree-split: 6f4929cda4f8218a38c7cd56e354872277d036ac

---
+++ src/utils/enums/ApiUrlPath.ts
@@ -0,0 +1,21 @@
+enum ApiUrlPath {
+  ActivateUser = '/activation',
+  AuthenticateUser = '/authentication',
+  CreateAnswer = '/create-answer',
+  CreateQuestion = '/create-question',
+  DeleteQuestion = '/delete-question',
+  GetActivationLink = '/activation-link',
+  GetQuestion = '/get-question',
+  GetQuestions = '/get-questions',
+  ReadInfo = '/info',
+  ReadStats = '/read-stats',
+  ReadUser = '/read-user',
+  RegisterUser = '/registration',
+  ReportQuestion = '/report-question',
+  UpdatePassword = '/update-password',
+  UpdateUser = '/update-user',
+  WatchQuestion = '/watch-question',
+  GetLogs = '/logs',
+}
+
+export default ApiUrlPath
