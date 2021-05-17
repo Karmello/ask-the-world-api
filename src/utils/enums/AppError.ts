9e4fbe5bf4e810535e07bd89076cc5b4240a1721
Karmello <nogakamil@vp.pl>
2021-05-17 23:15:11 +0200

    Squashed 'src/lib/temp/' content from commit 4a87402
    git-subtree-dir: src/lib/temp
    git-subtree-split: 4a87402734a63c7ea3efe9df38b3b2762f334c2f

---
+++ src/utils/enums/AppError.ts
@@ -0,0 +1,9 @@
+enum AppError {
+  SessionExpired = 'SESSION_EXPIRED_ERROR',
+  NoSuchUserError = 'NO_SUCH_USER_ERROR',
+  NoSuchQuestionError = 'NO_SUCH_QUESTION_ERROR',
+  SomethingWentWrong = 'SOMETHING_WENT_WRONG',
+  AlreadyFollowing = 'ALREADY_FOLLOWING',
+}
+
+export default AppError
