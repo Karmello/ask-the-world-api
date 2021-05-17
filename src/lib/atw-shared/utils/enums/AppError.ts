837c0995bdfb6fee583483b3e192f8897c5dc5bc
Karmello <nogakamil@vp.pl>
2021-05-17 23:01:02 +0200

    pull-shared

--- src/lib/atw-shared/utils/enums/AppError.ts
+++ src/lib/atw-shared/utils/enums/AppError.ts
@@ -3,6 +3,7 @@ enum AppError {
   NoSuchUserError = 'NO_SUCH_USER_ERROR',
   NoSuchQuestionError = 'NO_SUCH_QUESTION_ERROR',
   SomethingWentWrong = 'SOMETHING_WENT_WRONG',
+  AlreadyFollowing = 'ALREADY_FOLLOWING',
 }
 
 export default AppError
