e821aa43c6130d4824b372214ec76c2ad3e5dd1d
Karmello <nogakamil@vp.pl>
2021-05-18 22:13:29 +0200

    report

--- src/controllers/report/CreateReport/index.ts
+++ src/controllers/report/CreateReport/index.ts
@@ -1,25 +1,22 @@
 import { Application, Request, Response } from 'express'
 
 import { ApiUrlPath } from 'shared/utils/index'
-import { IQuestionDoc } from 'utils/index'
 import { userAuthMiddleware } from 'middleware/index'
-import { QuestionModel } from 'models/index'
+import { ReportModel } from 'models/index'
 
 export default (app: Application) =>
   app.post(ApiUrlPath.ReportQuestion, userAuthMiddleware, (req: Request, res: Response) => {
     //
-    const { _id } = req.query
+    const { questionId, reportReason } = req.query
 
-    QuestionModel.findOne({ _id }, (err, doc: IQuestionDoc) => {
-      //
-      if (err) return res.status(400).send(err)
-      if (!doc) return res.status(404).send()
-
-      doc
-        .save()
-        .then(_doc =>
-          res.status(200).send(QuestionModel.transformBeforeSend(_doc.toObject(), req.decoded._id))
-        )
-        .catch(_err => res.status(400).send(_err))
+    const report = new ReportModel({
+      questionId,
+      reporterId: req.decoded._id,
+      reason: reportReason,
     })
+
+    report
+      .save()
+      .then(doc => res.status(200).send(doc))
+      .catch(err => res.status(400).send(err))
   })
