3fa3a38a70a2a6ad82ccef77706d601b11dd116e
Kamil Noga <nogakamil@gmail.com>
2021-09-13 11:55:15 +0200

    Squashed 'src/lib/temp/' content from commit 512f8fa
    git-subtree-dir: src/lib/temp
    git-subtree-split: 512f8fa65947cc6cf1be95265130f5c7cf41b0ea

---
+++ src/utils/enums/Filter.ts
@@ -0,0 +1,12 @@
+enum Filter {
+  All = 'ALL',
+  Any = 'ANY',
+  Created = 'CREATED',
+  Answered = 'ANSWERED',
+  NotAnswered = 'NOT_ANSWERED',
+  Followed = 'FOLLOWED',
+  Top = 'TOP',
+  TopUsers = 'TOP_USERS',
+}
+
+export default Filter
