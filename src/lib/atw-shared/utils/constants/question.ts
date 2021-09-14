826678c5d62bf1f0c03dc893fb9c4f0985b79607
Kamil Noga <nogakamil@gmail.com>
2021-09-14 21:21:30 +0200

    ps

--- src/lib/atw-shared/utils/constants/question.ts
+++
@@ -1,8 +0,0 @@
-export const READ_QUESTIONS_MAX = 102
-export const READ_TOP_QUESTIONS_MAX = 12
-export const MIN_NUM_OF_ANSWERS = 2
-export const MAX_NUM_OF_ANSWERS = 8
-export const QUESTION_INPUT_MIN_LENGTH = 30
-export const QUESTION_INPUT_MAX_LENGTH = 100
-export const ANSWER_INPUT_MAX_LENGTH = 50
-export const QUESTION_KEYWORDS_MAX_LENGTH = 50
