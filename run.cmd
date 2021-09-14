26f5ab1ca32d7766e29e35f313108184f8b9a4a8
Kamil Noga <nogakamil@gmail.com>
2021-09-14 21:22:03 +0200

    Squashed 'src/lib/atw-shared/' content from commit 021456c8
    git-subtree-dir: src/lib/atw-shared
    git-subtree-split: 021456c8234f838b5556a64b2acaeca8df1daca0

---
+++ run.cmd
@@ -0,0 +1,24 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+SET ATW_CERTS_PATH="C:/Dev/certs"
+SET ATW_API_PATH="C:/Dev/ask-the-world-api"
+SET ATW_FE_PATH="C:/Dev/ask-the-world-fe"
+SET ATW_SHARED_PATH="C:/Dev/ask-the-world-shared"
+
+if %1 == app (
+  cmd /k -new_console:t:"ec2-master":d:%ATW_CERTS_PATH% ssh -i ask-the-world-dev.pem ubuntu@18.194.139.71
+  cmd /k -new_console:t:"ec2-feature":d:%ATW_CERTS_PATH% ssh -i ask-the-world-dev.pem ubuntu@18.196.70.139
+  cmd /k -new_console:t:"mongo" mongod
+  cmd /k -new_console:t:"api":d:%ATW_API_PATH% yarn start-local
+  cmd /k -new_console:t:"server":d:%ATW_FE_PATH% yarn dev-server
+  cmd /k -new_console:t:"client":d:%ATW_FE_PATH% yarn dev-client
+  cmd /k -new_console:t:"storybook":d:%ATW_FE_PATH% yarn storybook
+  cmd /k -new_console:t:"ask-the-world-fe":d:%ATW_FE_PATH% git status
+  cmd /k -new_console:t:"ask-the-world-api":d:%ATW_API_PATH% git status
+  cmd /k -new_console:t:"ask-the-world-shared":d:%ATW_SHARED_PATH% git status
+  exit
+  goto:end
+)
+
+:end
