627e9448e1b9e624c8a13fa12663f0407f378699
Kamil Noga <nogakamil@gmail.com>
2021-09-14 20:55:28 +0200

    sdf

--- cmd/pull-shared.cmd
+++ cmd/pull-shared.cmd
@@ -1,8 +1,9 @@
 @echo off
 
+call rimraf -rf "src/lib/temp"
+
 call git subtree add --prefix src/lib/temp https://gitlab.com/Karmello/ask-the-world-shared.git master --squash
 
 call rimraf -rf "src/lib/atw-shared"
 call xcopy  "src/lib/temp/src" "src/lib/atw-shared" /e /i /h
 call rimraf -rf "src/lib/atw-shared/**/spec.ts"
-call rimraf -rf "src/lib/temp"
