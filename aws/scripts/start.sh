85115cf23aba77ed1a9c254450713f74a3558806
Kamil Noga <nogakamil@gmail.com>
2021-10-07 20:49:47 +0200

    env setup

--- aws/scripts/start.sh
+++ aws/scripts/start.sh
@@ -2,14 +2,14 @@
 cd /home/ubuntu/ask-the-world-api
 
 if [ "$APPLICATION_NAME" == "ask-the-world-api-feature" ]
-then sudo pm2 start ecosystem.config.js --env feature
-
-else if [ "$APPLICATION_NAME" == "ask-the-world-api-master" ]
-then sudo pm2 start ecosystem.config.js --env master
-
-else if [ "$APPLICATION_NAME" == "ask-the-world-api-uat" ]
-then sudo pm2 start ecosystem.config.js --env uat
-
+then
+  sudo pm2 start ecosystem.config.js --env feature
+elif [ "$APPLICATION_NAME" == "ask-the-world-api-master" ]
+then
+  sudo pm2 start ecosystem.config.js --env master
+elif [ "$APPLICATION_NAME" == "ask-the-world-api-uat" ]
+then
+  sudo pm2 start ecosystem.config.js --env uat
 else 
   sudo pm2 start ecosystem.config.js --env prod
 fi
