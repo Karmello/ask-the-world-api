68ddc7127885dc96015d6cc26908121be9b0118e
Kamil Noga <nogakamil@gmail.com>
2021-10-10 03:42:47 +0200

    env

--- env/ecosystem.config.js
+++ env/ecosystem.config.js
@@ -6,25 +6,23 @@ module.exports = {
       env: {
         PORT: 9000,
         NODE_ENV: 'production',
+        API_URL: 'https://localhost:9000',
         EMAIL_USER: 'app.ask.the.world@gmail.com',
       },
       env_feature: {
         APP_ENV: 'remote-dev',
-        API_URL: 'https://localhost:9000',
         APP_URL: 'https://feature.ask-the-world.com',
         PAYPAL_API_URL: 'https://api-m.sandbox.paypal.com',
         DISABLE_PAYMENT: 'no',
       },
       env_master: {
         APP_ENV: 'remote-dev',
-        API_URL: 'https://localhost:9000',
         APP_URL: 'https://master.ask-the-world.com',
         PAYPAL_API_URL: 'https://api-m.sandbox.paypal.com',
         DISABLE_PAYMENT: 'no',
       },
       env_uat: {
         APP_ENV: 'remote-pre-prod',
-        API_URL: 'https://localhost:9000',
         APP_URL: 'https://uat.ask-the-world.com',
         PAYPAL_API_URL: 'https://api-m.paypal.com',
         DISABLE_PAYMENT: 'no',
