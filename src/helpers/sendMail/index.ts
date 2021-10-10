bde8000611b860830a128f966afbf1a1938fabfc
Kamil Noga <nogakamil@gmail.com>
2021-10-10 13:51:38 +0200

    mail

--- src/helpers/sendMail/index.ts
+++ src/helpers/sendMail/index.ts
@@ -12,9 +12,7 @@ export default (options: TOptions) =>
     const { EMAIL_USER, EMAIL_PASS } = process.env
 
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      service: 'gmail',
       auth: {
         user: EMAIL_USER,
         pass: EMAIL_PASS,
