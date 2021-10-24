918feade024730d3fc21690d5f36c06edfec2a1b
Kamil Noga <nogakamil@gmail.com>
2021-10-24 15:27:56 +0200

    debug off

--- env/ecosystem.config.js
+++ env/ecosystem.config.js
@@ -12,7 +12,6 @@ module.exports = {
       env_feature: {
         APP_ENV: 'remote-dev',
         DOMAIN: 'https://feature.ask-the-world.com',
-        DEBUG: 'engine,socket.io*',
       },
       env_master: {
         APP_ENV: 'remote-dev',
