9bb1ea5121f6cb13b2951912794574855bfc3459
Kamil Noga <nogakamil@gmail.com>
2022-08-08 16:38:56 +0000

    temp

---
+++ src/controllers/country/ReadCountries/index.ts
@@ -0,0 +1,26 @@
+import { Application, Request, Response } from 'express'
+
+import { ApiUrlPath, AppError } from 'atw-shared/utils/index'
+import { CountryModel } from 'models/index'
+
+export default (app: Application) =>
+  app.post(ApiUrlPath.Follow, (req: Request, res: Response) => {
+      //
+      FollowModel.findOne({ questionId: req.query._id, followerId: req.decoded._id })
+        .then(doc => {
+          // already following
+          if (doc) return res.status(400).send(AppError.AlreadyFollowing)
+
+          const follow = new FollowModel({
+            questionId: req.query._id,
+            followerId: req.decoded._id,
+          })
+
+          follow
+            .save()
+            .then(_doc => res.status(200).send(_doc))
+            .catch(err => res.status(400).send(err))
+        })
+        .catch(err => res.status(400).send(err))
+    }
+  )
