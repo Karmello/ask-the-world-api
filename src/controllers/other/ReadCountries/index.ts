5838b20aa5897c87d18ed9870504c4442d4f2bb2
Kamil Noga <nogakamil@gmail.com>
2022-08-08 19:47:40 +0000

    ref

--- src/controllers/other/ReadCountries/index.ts
+++ src/controllers/other/ReadCountries/index.ts
@@ -3,7 +3,7 @@ import { Application, Request, Response } from 'express'
 import { ApiUrlPath } from 'atw-shared/utils/index'
 import { CountryModel } from 'models/index'
 
-export default (app: Application) =>
+export default (app: Application) => {
   app.get(ApiUrlPath.Countries, (req: Request, res: Response) => {
     CountryModel.find()
         .then(docs => {
@@ -12,3 +12,4 @@ export default (app: Application) =>
         .catch(err => res.status(400).send(err))
     }
   )
+}
--- src/models/Country/Country.ts
+++ src/models/Country/Country.ts
@@ -6,6 +6,10 @@ const { model, Schema } = mongoose
 
 const countrySchema = new Schema(
   {
+    _id: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
