b22553c515eefdf629b53e60890eaad0af7d9069
Kamil Noga <nogakamil@gmail.com>
2022-08-23 23:08:06 +0000

    Squashed 'src/ext/atw-shared/' changes from 7fab057..94d7298
    94d7298 consts
    
    git-subtree-dir: src/ext/atw-shared
    git-subtree-split: 94d7298ceab78e58b465295a0d21f7dc317d2b05

--- source/utils/constants/question.ts
+++ source/utils/constants/question.ts
@@ -1,5 +1,5 @@
 export const READ_QUESTIONS_MAX = 48
-export const READ_TOP_QUESTIONS_MAX = 12
+export const READ_TOP_QUESTIONS_MAX = 48
 export const MIN_NUM_OF_ANSWERS = 2
 export const MAX_NUM_OF_ANSWERS = 10
 export const QUESTION_INPUT_MIN_LENGTH = 15
