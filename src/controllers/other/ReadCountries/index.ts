6371c43776ef1b4c4a937c36627b89fd987bcd45
Kamil Noga <nogakamil@gmail.com>
2022-09-02 22:04:15 +0000

    sdfsdf

--- src/controllers/other/ReadCountries/index.ts
+++ src/controllers/other/ReadCountries/index.ts
@@ -2,13 +2,23 @@ import { Application, Request, Response } from 'express'
 
 import { ApiUrlPath } from 'atw-shared/utils/index'
 import { CountryModel } from 'models/index'
+import msgs from 'utils/msgs'
 
 export default (app: Application) => {
   app.get(ApiUrlPath.Countries, (req: Request, res: Response) => {
+    return res.status(400).send({
+      msg: msgs.SOMETHING_WENT_WRONG,
+    })
     CountryModel.find()
       .then(docs => {
-        res.status(200).send(docs)
+        res.status(200).send({
+          countries: docs,
+        })
       })
-      .catch(err => res.status(400).send(err))
+      .catch(() => {
+        res.status(400).send({
+          msg: msgs.SOMETHING_WENT_WRONG,
+        })
+      })
   })
 }
--- src/controllers/other/ReadStats/index.ts
+++ src/controllers/other/ReadStats/index.ts
@@ -2,8 +2,9 @@ import { Application, Request, Response } from 'express'
 
 import { ApiUrlPath } from 'atw-shared/utils/index'
 import { UserModel, QuestionModel, AnswerModel } from 'models/index'
+import msgs from 'utils/msgs'
 
-export default (app: Application) =>
+export default (app: Application) => {
   app.get(ApiUrlPath.Stats, (req: Request, res: Response) => {
     Promise.all([
       UserModel.countDocuments(),
@@ -19,8 +20,11 @@ export default (app: Application) =>
           },
         })
       },
-      err => {
-        res.status(400).send(err)
+      () => {
+        res.status(400).send({
+          msgs: msgs.SOMETHING_WENT_WRONG,
+        })
       }
     )
   })
+}
