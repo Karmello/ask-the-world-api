e23b6221ee38bbee6afe5b710aeda9cb8dda1164
Kamil Noga <nogakamil@gmail.com>
2022-09-10 07:22:45 +0000

    update

--- src/controllers/answer/UpdateAnswer/index.ts
+++ src/controllers/answer/UpdateAnswer/index.ts
@@ -2,9 +2,9 @@ import { Application, Request, Response } from 'express'
 
 import { ApiUrlPath } from 'atw-shared/utils/index'
 import { AnswerModel, QuestionModel } from 'models/index'
-import msgs from 'utils/msgs'
 import { readAuthToken } from 'middleware/index'
 import { checkSelectedIndexes } from 'validation/index'
+import msgs from 'utils/msgs'
 
 import checkRequest from './checkRequest'
 
@@ -14,51 +14,39 @@ export default (app: Application) => {
     readAuthToken,
     checkRequest,
     (req: Request, res: Response) => {
-      AnswerModel.findOne({
-        answererId: req.decoded._id,
-        questionId: req.query.questionId,
-      })
-        .then(answer => {
-          if (!answer) {
+      QuestionModel.findOne({ _id: req.query.questionId })
+        .then(question => {
+          if (!question) {
             res.status(400).send({
               msg: msgs.SOMETHING_WENT_WRONG,
             })
           } else {
-            QuestionModel.findOne({ _id: req.query.questionId })
-              .then(question => {
-                if (!question) {
+            if (!checkSelectedIndexes(req.body, question)) {
+              res.status(400).send({
+                msg: msgs.SOMETHING_WENT_WRONG,
+              })
+            } else {
+              AnswerModel.findOneAndUpdate(
+                {
+                  answererId: req.decoded._id,
+                  questionId: req.query.questionId,
+                },
+                {
+                  answeredAt: Date.now(),
+                  selectedIndexes: req.body,
+                }
+              )
+                .then(answer => {
+                  res.status(200).send({
+                    answer,
+                  })
+                })
+                .catch(() => {
                   res.status(400).send({
                     msg: msgs.SOMETHING_WENT_WRONG,
                   })
-                } else {
-                  if (!checkSelectedIndexes(req.body, question)) {
-                    res.status(400).send({
-                      msg: msgs.SOMETHING_WENT_WRONG,
-                    })
-                  } else {
-                    answer.answeredAt = Date.now()
-                    answer.selectedIndexes = req.body
-
-                    answer
-                      .save()
-                      .then(() => {
-                        res.status(200).send({
-                          answer,
-                        })
-                      })
-                      .catch(() => {
-                        res.status(400).send({
-                          msg: msgs.SOMETHING_WENT_WRONG,
-                        })
-                      })
-                  }
-                }
-              })
-              .catch(() => {
-                res.status(400).send({
-                  msg: msgs.SOMETHING_WENT_WRONG,
                 })
-              })
+            }
           }
         })
         .catch(() => {
