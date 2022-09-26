d731f7e880497c88fdd8d949fa10629419540124
Kamil Noga <nogakamil@gmail.com>
2022-09-26 12:25:09 +0000

    validation

--- src/validation/checkSelectedIndexes/index.ts
+++ src/validation/checkSelectedIndexes/index.ts
@@ -1,17 +1,36 @@
 import { IQuestion } from 'atw-shared/utils'
 
 export default (selectedIndexes: number[], question: IQuestion): boolean => {
-  // const {
-  //   answers,
-  //   options: { maxSelectable },
-  // } = question
-  // return (
-  //   selectedIndexes.length >= 1 &&
-  //   selectedIndexes.length <= maxSelectable &&
-  //   !selectedIndexes.some(n => {
-  //     return n < 0 || n > answers.length - 1
-  //   })
-  // )
+  const {
+    answers,
+    numOfVotes: { exact, range },
+  } = question
+
+  if (!selectedIndexes?.length) {
+    return false
+  }
+
+  if (new Set(selectedIndexes).size !== selectedIndexes.length) {
+    return false
+  }
+
+  if (exact !== undefined) {
+    if (selectedIndexes.length !== exact) {
+      return false
+    }
+  } else if (range !== undefined) {
+    if (selectedIndexes.length < range.min || selectedIndexes.length > range.max) {
+      return false
+    }
+  }
+
+  if (
+    selectedIndexes.some(n => {
+      return n < 0 || n > answers.length - 1
+    })
+  ) {
+    return false
+  }
 
   return true
 }
