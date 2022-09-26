5bc2077056cc54e83268cab0864391d3a33c8ffd
Kamil Noga <nogakamil@gmail.com>
2022-09-26 15:13:23 +0000

    fix

--- tests/deactivateUser.spec.ts
+++ tests/deactivateUser.spec.ts
@@ -13,37 +13,37 @@ describe('deactivateUser', () => {
   let mailToken: string
 
   before(done => {
-    UserModel.collection.insertOne({
-      _id: userId,
-      email: 'username1@email.com',
-      username,
-      password: '$2a$10$8kcJdl16EMupO2cmLFAsf.jVWkFaCV5W47Mip6BMfwUJjLM6/J0n2',
-      dateOfBirth: '1995-02-15',
-      country: 'PL',
-      sex: 'M',
-      config: {
-        registeredAt: Date.now(),
-        confirmed: true,
-        payment: {},
-      },
+    Promise.all([
+      UserModel.collection.insertOne({
+        _id: userId,
+        email: 'username1@email.com',
+        username,
+        password: '$2a$10$8kcJdl16EMupO2cmLFAsf.jVWkFaCV5W47Mip6BMfwUJjLM6/J0n2',
+        dateOfBirth: '1995-02-15',
+        country: 'PL',
+        sex: 'M',
+        config: {
+          registeredAt: Date.now(),
+          confirmed: true,
+          payment: {},
+        },
+      }),
+      QuestionModel.collection.insertOne({
+        _id: questionId,
+        creatorId: userId,
+        text: 'Who is going to be a new President of the United States of America ?',
+        answers: ['Donald Trump', 'Joe Biden'],
+        numOfVotes: { exact: 1 },
+        isStopped: false,
+      }),
+      AnswerModel.collection.insertOne({
+        questionId,
+        answererId: userId,
+        selectedIndexes: [0],
+      }),
+    ]).then(() => {
+      done()
     })
-
-    QuestionModel.collection.insertOne({
-      _id: questionId,
-      creatorId: userId,
-      text: 'Who is going to be a new President of the United States of America ?',
-      answers: ['Donald Trump', 'Joe Biden'],
-      numOfVotes: { exact: 1 },
-      isStopped: false,
-    })
-
-    AnswerModel.collection.insertOne({
-      questionId,
-      answererId: userId,
-      selectedIndexes: [0],
-    })
-
-    done()
   })
 
   it('user exists', done => {
