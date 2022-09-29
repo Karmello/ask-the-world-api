b4017a0a16c82edb40c2b4cc0d90ddf3385e1ff1
Kamil Noga <nogakamil@gmail.com>
2022-09-29 21:01:30 +0000

    categories

--- db/data/question-categories.ts
+++ db/data/question-categories.ts
@@ -4,6 +4,10 @@ export default [
     name: 'Architecture',
   },
   {
+    _id: 'arts',
+    name: 'Arts',
+  },
+  {
     _id: 'banking',
     name: 'Banking',
   },
@@ -12,20 +16,68 @@ export default [
     name: 'Business',
   },
   {
+    _id: 'cars',
+    name: 'Cars',
+  },
+  {
+    _id: 'construction',
+    name: 'Construction',
+  },
+  {
     _id: 'economics',
     name: 'Economics',
   },
   {
-    _id: 'food_and_beverage',
-    name: 'Food & Beverage',
+    _id: 'electronics',
+    name: 'Electronics',
+  },
+  {
+    _id: 'film',
+    name: 'Film',
+  },
+  {
+    _id: 'food_and_beverages',
+    name: 'Food & beverages',
   },
   {
     _id: 'government',
     name: 'Government',
   },
   {
+    _id: 'history',
+    name: 'History',
+  },
+  {
+    _id: 'internet',
+    name: 'Internet',
+  },
+  {
+    _id: 'languages',
+    name: 'Languages',
+  },
+  {
+    _id: 'literature',
+    name: 'Literature',
+  },
+  {
     _id: 'music',
     name: 'Music',
+  },
+  {
+    _id: 'nature',
+    name: 'Nature',
+  },
+  {
+    _id: 'politics',
+    name: 'Politics',
+  },
+  {
+    _id: 'programming',
+    name: 'Programming',
+  },
+  {
+    _id: 'religion',
+    name: 'Religion',
   },
   {
     _id: 'science',
@@ -38,5 +90,13 @@ export default [
   {
     _id: 'technology',
     name: 'Technology',
+  },
+  {
+    _id: 'tv',
+    name: 'TV',
+  },
+  {
+    _id: 'other',
+    name: 'Other',
   },
 ]
