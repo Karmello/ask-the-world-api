674faf465f4b8131f563032c0736c9e9f218aa49
Kamil Noga <nogakamil@gmail.com>
2022-10-05 18:17:18 +0000

    save

--- tests/activateUser.spec.ts
+++ tests/activateUser.spec.ts
@@ -1,20 +1,17 @@
 import mongoose from 'mongoose'
 
-import { ApiUrlPath, IUser, X_AUTH_TOKEN } from '../src/ext/atw-shared/source/utils'
+import { ApiUrlPath, X_AUTH_TOKEN } from '../src/ext/atw-shared/source/utils'
 import msgs from '../src/utils/msgs'
 import { api, chai, expect } from './'
 import { getFreshAuthToken } from '../src/helpers/index'
 import { UserModel } from '../src/models'
 
+const _id = new mongoose.Types.ObjectId('5eeb976152c1dc555c2d676c')
+
 describe('activateUser', () => {
   it('should responde with NO_SUCH_USER', done => {
-    const mailToken = getFreshAuthToken(
-      {
-        _id: new mongoose.Types.ObjectId('5eeb976152c1dc555c2d676c'),
-        config: { confirmed: true },
-      } as unknown as IUser,
-      true
-    )
+    const user = { _id, config: { confirmed: true } }
+    const mailToken = getFreshAuthToken(user as never, true)
     chai
       .request(api)
       .get(ApiUrlPath.UserActivate)
@@ -27,76 +24,36 @@ describe('activateUser', () => {
   })
 
   it('should responde with EMAIL_ALREADY_CONFIRMED', done => {
-    UserModel.collection
-      .insertOne({
-        _id: new mongoose.Types.ObjectId('5eeb976152c1dc555c2d676c'),
-        email: 'username1@email.com',
-        username: 'username1',
-        password: '$2a$10$8kcJdl16EMupO2cmLFAsf.jVWkFaCV5W47Mip6BMfwUJjLM6/J0n2',
-        dateOfBirth: '1995-02-15',
-        country: 'PL',
-        sex: 'M',
-        config: {
-          registeredAt: Date.now(),
-          confirmed: true,
-          payment: {},
-        },
-      })
-      .then(() => {
-        const mailToken = getFreshAuthToken(
-          {
-            _id: new mongoose.Types.ObjectId('5eeb976152c1dc555c2d676c'),
-            config: { confirmed: true },
-          } as unknown as IUser,
-          true
-        )
-        chai
-          .request(api)
-          .get(ApiUrlPath.UserActivate)
-          .set(X_AUTH_TOKEN, mailToken)
-          .end((err, res) => {
-            expect(res.status).to.eql(403)
-            expect(res.text).to.eql(msgs.EMAIL_ALREADY_CONFIRMED.text)
-            UserModel.collection.deleteMany({})
-            done()
-          })
-      })
+    const user = { _id, config: { confirmed: true } }
+    UserModel.collection.insertOne(user).then(() => {
+      const mailToken = getFreshAuthToken(user as never, true)
+      chai
+        .request(api)
+        .get(ApiUrlPath.UserActivate)
+        .set(X_AUTH_TOKEN, mailToken)
+        .end((err, res) => {
+          expect(res.status).to.eql(403)
+          expect(res.text).to.eql(msgs.EMAIL_ALREADY_CONFIRMED.text)
+          UserModel.collection.deleteMany({})
+          done()
+        })
+    })
   })
 
   it('should responde with EMAIL_CONFIRMED', done => {
-    UserModel.collection
-      .insertOne({
-        _id: new mongoose.Types.ObjectId('5eeb976152c1dc555c2d676c'),
-        email: 'username1@email.com',
-        username: 'username1',
-        password: '$2a$10$8kcJdl16EMupO2cmLFAsf.jVWkFaCV5W47Mip6BMfwUJjLM6/J0n2',
-        dateOfBirth: '1995-02-15',
-        country: 'PL',
-        sex: 'M',
-        config: {
-          registeredAt: Date.now(),
-          confirmed: false,
-          payment: {},
-        },
-      })
-      .then(() => {
-        const mailToken = getFreshAuthToken(
-          {
-            _id: new mongoose.Types.ObjectId('5eeb976152c1dc555c2d676c'),
-            config: { confirmed: false },
-          } as unknown as IUser,
-          true
-        )
-        chai
-          .request(api)
-          .get(ApiUrlPath.UserActivate)
-          .set(X_AUTH_TOKEN, mailToken)
-          .end((err, res) => {
-            expect(res.status).to.eql(200)
-            expect(res.text).to.eql(msgs.EMAIL_CONFIRMED.text)
-            UserModel.collection.deleteMany({})
-            done()
-          })
-      })
+    const user = { _id, config: { confirmed: false } }
+    UserModel.collection.insertOne(user).then(() => {
+      const mailToken = getFreshAuthToken(user as never, true)
+      chai
+        .request(api)
+        .get(ApiUrlPath.UserActivate)
+        .set(X_AUTH_TOKEN, mailToken)
+        .end((err, res) => {
+          expect(res.status).to.eql(200)
+          expect(res.text).to.eql(msgs.EMAIL_CONFIRMED.text)
+          UserModel.collection.deleteMany({})
+          done()
+        })
+    })
   })
 })
