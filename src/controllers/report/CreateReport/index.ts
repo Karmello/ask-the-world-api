7342661cfff31c187a0cd6bae69b5006c7958032
Kamil Noga <nogakamil@gmail.com>
2023-08-06 03:21:22 +0000

    reporting

--- src/controllers/report/CreateReport/index.ts
+++ src/controllers/report/CreateReport/index.ts
@@ -1,7 +1,7 @@
 import { Application, Request, Response } from 'express'
 
 import { ApiUrlPath } from 'atw-shared/utils/index'
-import { ReportModel } from 'models/index'
+import { ReportModel, QuestionModel } from 'models/index'
 import msgs from 'utils/msgs'
 import { readAuthToken, checkAuthToken } from 'middleware/index'
 
@@ -13,31 +13,46 @@ export default (app: Application) => {
     (req: Request, res: Response) => {
       const { questionId, reportReasons } = req.query
 
-      ReportModel.findOneAndUpdate(
-        {
-          questionId,
-          reporterId: req.decoded._id,
-        },
-        {
-          questionId,
-          reporterId: req.decoded._id,
-          reasons: (reportReasons as string).split('-'),
-        },
-        {
-          upsert: true,
-          new: true,
-          runValidators: true,
-        }
-      )
+      QuestionModel.findOne({ _id: questionId })
         .then(doc => {
-          res.status(200).send({
-            report: doc,
-            msg: msgs.QUESTION_REPORTED,
-          })
+          if (!doc) {
+            return res.status(400).send({
+              msg: msgs.QUESTION_MUST_HAVE_BEEN_DELETED,
+            })
+          }
+
+          ReportModel.findOneAndUpdate(
+            {
+              questionId,
+              reporterId: req.decoded._id,
+            },
+            {
+              questionId,
+              reporterId: req.decoded._id,
+              reasons: (reportReasons as string).split('-'),
+            },
+            {
+              upsert: true,
+              new: true,
+              runValidators: true,
+            }
+          )
+            .then(doc => {
+              console.log(doc)
+              res.status(200).send({
+                report: doc,
+                msg: msgs.QUESTION_REPORTED,
+              })
+            })
+            .catch(() => {
+              res.status(400).send({
+                msg: msgs.QUESTION_MUST_HAVE_BEEN_DELETED,
+              })
+            })
         })
         .catch(() => {
           res.status(400).send({
-            msg: msgs.SOMETHING_WENT_WRONG,
+            msg: msgs.QUESTION_MUST_HAVE_BEEN_DELETED,
           })
         })
     }
