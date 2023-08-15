ba31ed02f95942fc80609dabf425b1486aa083c5
Kamil Noga <nogakamil@gmail.com>
2023-08-15 06:56:47 +0000

    Squashed 'src/ext/atw-shared/' changes from c9ed359e..e0db2a33
    e0db2a33 short x auth token expires in
    
    git-subtree-dir: src/ext/atw-shared
    git-subtree-split: e0db2a33830359db6da72b39e0ecb138761bde2b

--- source/utils/constants/app.ts
+++ source/utils/constants/app.ts
@@ -1,5 +1,6 @@
 export const X_AUTH_TOKEN = 'atw-x-auth-token'
 export const X_AUTH_TOKEN_EXPIRES_IN = 86400
+export const X_AUTH_TOKEN_SHORT_EXPIRES_IN = 300
 
 export const DATE_FORMAT = 'yyyy-MM-dd'
 export const DATE_TIME_FORMAT = 'yyyy-MM-dd H:mm:ss'
